bdaa4d0ac7a24d65a114d1471636b73f9cb51a5c
okkindel <ebrithil97@gmail.com>
2019-01-17 01:13:42 +0100

    new particles options

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 var canvas, context;
 var particles = [];
 var gravity = 0.5;
+var quantity = 200;
 var floor;
-var checkColl = true;
+var checkColl = false;
 
 window.onload = function () {
     canvas = document.getElementById('canvas');
@@ -43,17 +44,29 @@ function update() {
     });
     text("Click on screen to place a ball", "2rem Arial", canvas.width / 2, canvas.height / 4, 'center', 'black');
     text("Press C to turn ball collision " + (checkColl == true ? "off" : "on") + ".", "1rem Arial", canvas.width - 20, 30, 'end', 'black');
+    text("Number of particles: " + particles.length, "1rem Arial", 20, 30, 'start', 'black');
     text("Press arrows to modify gravity, current: " + gravity + ".", "1rem Arial", canvas.width - 20, 50, 'end', 'black');
+    text("Press arrows to modify spawn timeout, current: " + quantity + ".", "1rem Arial", canvas.width - 20, 70, 'end', 'black');
     text('"Some balls just want to fly away."', "1rem Arial", canvas.width - 20, canvas.height - 40, 'end', 'white');
     text("- Me", "1rem Arial", canvas.width - 20, canvas.height - 20, 'end', 'white');
 }
 
 document.addEventListener('mousedown', function () {
-    const particle = new Particle(event.clientX, event.clientY);
-    particle.addForceX(Math.random() - 0.5);
-    particle.addForceY(Math.random() * (-10));
-    particles.push(particle);
+    mouse = true; callEvent(event.clientX, event.clientY);
 });
+document.addEventListener('mouseup', function () {
+    mouse = false;
+});
+
+function callEvent(x, y) {
+    if (mouse) {
+        const particle = new Particle(x, y);
+        particle.addForceX(Math.random() - 0.5);
+        particle.addForceY(Math.random() * (-10));
+        particles.push(particle);
+        setTimeout("callEvent(" + x + "," + y + ")", quantity);
+    } else return;
+}
 
 document.addEventListener('keydown', function () {
     if (event.keyCode == 67) {
@@ -64,5 +77,11 @@ document.addEventListener('keydown', function () {
     }
     if (event.keyCode == 40 && gravity > -2) {
         gravity -= 0.125;
+    }
+    if (event.keyCode == 37 && quantity > 0) {
+        quantity -= 10;
+    }
+    if (event.keyCode == 39 && quantity < 400) {
+        quantity += 10;
     }
 });
